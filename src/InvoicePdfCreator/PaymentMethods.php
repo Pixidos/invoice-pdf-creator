4eccef88afad792506dadb3a7fc2414480aecb98
Ondra Votava <ja@ondravotava.cz>
2016-03-24 09:35:37 +0100

    add PaymentMethods wrapper for constants

---
+++ src/InvoicePdfCreator/PaymentMethods.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * This file is part of the Pixidos (http://www.pixidos.com)
+ *
+ * Copyright (c) 2016 Ondra Votava Pixidos LTD  (ondra.votava@pixidos.com)
+ *
+ * For the full copyright and license information, please view the file license.txt that was distributed with this source code.
+ *
+ */
+
+/**
+ * Created by PhpStorm.
+ * User: Ondra Votava
+ * Date: 14.03.2016
+ * Time: 8:25
+ */
+
+namespace Pixidos\InvoicePdfCreator;
+
+/**
+ * Class PaymentMethods
+ * @package Pixidos\InvoicePdfCreator
+ * @author Ondra Votava <ondra.votava@pixidos.com>
+ */
+
+class PaymentMethods
+{
+
+    const BANK_ACC = 'bankacc';
+    const CASH = 'cash';
+    const CASH_ON_DELIVERY = 'cashOnDelivery';
+}
